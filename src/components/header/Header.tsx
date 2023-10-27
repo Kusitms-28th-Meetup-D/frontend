dd3f883a6e03796c7d257f2bd7d714288c21466a
j2noo <wlsdndml213@naver.com>
2023-10-27 16:23:41 +0900

    feat(#3) : 헤더와 로그인페이지 연결 git push

--- src/components/header/Header.tsx
+++ src/components/header/Header.tsx
@@ -1,4 +1,33 @@
+import styled from 'styled-components';
+import { Link } from 'react-router-dom';
 const Header = () => {
-  return <div>i am header</div>;
+  return (
+    <HeaderLayout>
+      <HeaderItem>로고</HeaderItem>
+      <HeaderItem>공모전 리스트</HeaderItem>
+      <HeaderItem>내팀</HeaderItem>
+      <HeaderItem>마이페이지</HeaderItem>
+      <LoginLink to={'/login'}>로그인</LoginLink>
+    </HeaderLayout>
+  );
 };
 export default Header;
+
+const HeaderLayout = styled.header`
+  //width: 153.6rem;
+  min-width: 128rem;
+  height: 10rem;
+  background-color: ${(props) => props.theme.colors.blue1};
+
+  display: flex;
+  justify-content: space-around;
+  align-items: center;
+  gap: 3rem;
+`;
+const HeaderItem = styled.div`
+  font-size: 2rem;
+`;
+const LoginLink = styled(Link)`
+  font-size: 2rem;
+  cursor: pointer;
+`;
