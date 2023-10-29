2520f981074193f26c2e9897ce492a80a5855125
j2noo <wlsdndml213@naver.com>
2023-10-30 01:50:12 +0900

    feat(#3) : 로그아웃 로직

--- src/components/logout/KakaoLogout.tsx
+++ src/components/logout/KakaoLogout.tsx
@@ -1,17 +1,10 @@
 export const kakao = (window as any).Kakao;
 
 const KakaoLogout = () => {
-  const redirectUri = 'http://localhost:5173/login/oauth';
   return (
     <img
       src="assets/images/header/kakaoLogout.png"
-      onClick={() =>
-        kakao.Auth.authorize({
-          redirectUri: `${redirectUri}`,
-          scope: 'profile_nickname,profile_image,account_email',
-          // prompt: "select_account",
-        })
-      }
+      onClick={() => kakao.Auth.logout()}
     ></img>
   );
 };
