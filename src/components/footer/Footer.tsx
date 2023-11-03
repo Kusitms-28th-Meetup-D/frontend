720d5d2de9c5f11f9a8cd8fbe8b0997ce7319647
j2noo <wlsdndml213@naver.com>
2023-11-04 00:03:41 +0900

    feat(#13) : 푸터에서 폰트 테스트

--- src/components/footer/Footer.tsx
+++ src/components/footer/Footer.tsx
@@ -1,10 +1,12 @@
 import styled from 'styled-components';
 
 const Footer = () => {
-  return <Test>이거 프리텐다드맞아? 앓앓앍만밝붹뷁쫧   i am asdwq asdxwqeZQA</Test>;
+  return (
+    <Test>이거 프리텐다드맞아? 앓앓앍만밝붹뷁쫧 i am asdwq asdxwqeZQA</Test>
+  );
 };
 export default Footer;
 
 const Test = styled.div`
-  ${(props) => props.theme.fonts.heading6}
+  ${(props) => props.theme.fonts.heading5}
 `;
