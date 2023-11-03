e57083b3244292e9d2d129450854c4034eed0614
j2noo <wlsdndml213@naver.com>
2023-11-03 23:49:32 +0900

    feat(#12) : theme.ts 및 타입 수정

--- src/styles/globalFont.ts
+++ src/styles/globalFont.ts
@@ -1,6 +1,6 @@
 import { createGlobalStyle } from 'styled-components';
 import PRETENDARD from '/assets/fonts/PretendardVariable.woff2';
-import SUIT from '/assets/fonts/PSuitVariable.woff2';
+import SUIT from '/assets/fonts/SuitVariable.woff2';
 
 const GlobalFont = createGlobalStyle`    
   @font-face {
