b2bb08b4bb5b6312c7277c0d2fbfb8b37884f30f
j2noo <wlsdndml213@naver.com>
2023-11-03 21:44:33 +0900

    feat(#11) : 프로필 페이지 라우팅 설정

--- src/Router.tsx
+++ src/Router.tsx
@@ -3,6 +3,7 @@ import Header from './components/header/Header';
 import Footer from './components/footer/Footer';
 import Oauth from './pages/login/Oauth';
 import Join from './pages/join/Join';
+import Profile from './pages/profile/Profile';
 
 function Router() {
 
@@ -14,6 +15,7 @@ function Router() {
         <Route path="/login" element={<div>login</div>} />
         <Route path="/login/oauth" element={<Oauth />} />
         <Route path="/login/join" element={<Join />} />
+        <Route path="profile/:userid" element={<Profile/>}/>
       </Routes>
       <Footer />
     </BrowserRouter>
---
+++ src/pages/profile/Profile.tsx
@@ -0,0 +1,5 @@
+const Profile = () => {
+  return <div>s</div>;
+};
+
+export default Profile;
