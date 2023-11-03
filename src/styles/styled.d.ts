06f48e33da8fa86c31ec8aced575c5907029ac4d
j2noo <wlsdndml213@naver.com>
2023-11-03 22:59:37 +0900

    fix(#12) : styled.d.ts 정의파일 수정

--- src/styles/styled.d.ts
+++ src/styles/styled.d.ts
@@ -1,11 +1,13 @@
 //styled.d.ts
 //theme.ts를 설명해주는 파일?
 import 'styled-components';
-import { ColorsTypes } from './theme';
+import { ColorsTypes, DevicesTypes, FontsTypes } from './theme';
 
 // and extend them!
 declare module 'styled-components' {
   export interface DefaultTheme {
     colors: ColorsTypes;
+    fonts: FontsTypes;
+    devices: DevicesTypes;
   }
 }
