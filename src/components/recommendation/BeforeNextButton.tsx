baa5d39b5420aaa20653a9915a1d096c950c94a4
parkyejin <uiop5809@naver.com>
2023-11-06 05:34:10 +0900

    feat(#8): 추천사 페이지 하단 버튼

---
+++ src/components/recommendation/BeforeNextButton.tsx
@@ -0,0 +1,71 @@
+import { useNavigate } from 'react-router-dom';
+import { styled } from 'styled-components';
+
+interface BeforeNextButtonProps {
+  next: string;
+  route: string;
+}
+
+const BeforeNextButton = ({ next, route }: BeforeNextButtonProps) => {
+  const navigate = useNavigate();
+
+  const handleNextButtonClick = () => {
+    navigate(`${route}`);
+  };
+
+  return (
+    <ButtonBox>
+      <BeforeButton>
+        <img
+          src="/assets/images/recommendation/beforeArrow.svg"
+          alt="arrow-before"
+        />
+        이전
+      </BeforeButton>
+      <NextButton onClick={handleNextButtonClick}>
+        {next}
+        <img
+          src="/assets/images/recommendation/nextArrow.svg"
+          alt="arrow-right"
+        />
+      </NextButton>
+    </ButtonBox>
+  );
+};
+
+export default BeforeNextButton;
+
+const ButtonBox = styled.div`
+  display: flex;
+  gap: 2rem;
+  justify-content: center;
+  margin: 8rem 0 10rem;
+`;
+
+const BeforeButton = styled.button`
+  width: 17rem;
+  height: 5.5rem;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  gap: 1.5rem;
+  background: ${({ theme }) => theme.colors.gray10};
+  color: ${({ theme }) => theme.colors.gray40};
+  ${({ theme }) => theme.fonts.buttonL};
+  border: 1px solid ${({ theme }) => theme.colors.gray20};
+  border-radius: 3.6rem;
+`;
+
+const NextButton = styled.button`
+  width: 17rem;
+  height: 5.5rem;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  gap: 1.5rem;
+  background: ${({ theme }) => theme.colors.primary60};
+  color: ${({ theme }) => theme.colors.white};
+  ${({ theme }) => theme.fonts.buttonL};
+  border: 1px solid ${({ theme }) => theme.colors.gray20};
+  border-radius: 3.6rem;
+`;
