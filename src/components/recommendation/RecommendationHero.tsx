cfa416514cd8f03b76cbcc6fa38f8dd51ed418c4
parkyejin <uiop5809@naver.com>
2023-11-06 05:35:39 +0900

    feat(#8): 추천사 남길 주인공 박스

---
+++ src/components/recommendation/RecommendationHero.tsx
@@ -0,0 +1,35 @@
+import { styled } from 'styled-components';
+import HeroBox from './HeroBox';
+import { heroList } from '../../constants/HeroList';
+
+const RecommendationHero = () => {
+  return (
+    <>
+      <HeroTitle>추천사를 남길 주인공은?</HeroTitle>
+      <HeroLayout>
+        {heroList.map((hero) => (
+          <HeroBox key={hero.name} name={hero.name} src={hero.src} />
+        ))}
+      </HeroLayout>
+    </>
+  );
+};
+
+export default RecommendationHero;
+
+const HeroTitle = styled.div`
+  ${({ theme }) => theme.fonts.heading3};
+  text-align: center;
+  margin: 4rem 0 5rem 0;
+`;
+
+const HeroLayout = styled.div`
+  display: grid;
+  grid-template-columns: repeat(2, 1fr);
+  grid-gap: 20px;
+  justify-items: center;
+  align-items: center;
+  justify-content: center;
+  margin: auto;
+  padding: 0 15rem;
+`;
