085de80b4d862ec7fbcb860c91dc21a6022c8be4
parkyejin <uiop5809@naver.com>
2023-11-07 14:29:25 +0900

    feat(#17): 페이지네이션 공통 컴포넌트

---
+++ src/components/common/Pagination.tsx
@@ -0,0 +1,67 @@
+import { styled } from 'styled-components';
+
+interface PaginationProps {
+  totalPage: number;
+  currentPage: number;
+  setCurrentPage: (page: number) => void;
+}
+
+const Pagination = ({
+  totalPage,
+  currentPage,
+  setCurrentPage,
+}: PaginationProps) => {
+  const pages = [];
+  for (let p = 1; p <= totalPage; p++) {
+    pages.push(p);
+  }
+
+  return (
+    <PaginationWrapper>
+      {pages.map((page) => (
+        <PaginationButton
+          key={page}
+          active={currentPage === page}
+          onClick={() => setCurrentPage(page)}
+        >
+          {page}
+        </PaginationButton>
+      ))}
+    </PaginationWrapper>
+  );
+};
+
+export default Pagination;
+
+const PaginationWrapper = styled.div`
+  display: flex;
+  justify-content: flex-end;
+  gap: 0.8rem;
+`;
+
+const PaginationButton = styled.button<{ active?: boolean }>`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  width: 2rem;
+  height: 2rem;
+  padding: 5px 10px;
+  background-color: ${({ active, theme }) =>
+    active ? theme.colors.primary40 : theme.colors.gray20};
+  color: ${({ active, theme }) =>
+    active ? theme.colors.white : theme.colors.gray90};
+  cursor: pointer;
+  border-radius: 7px;
+
+  &:disabled {
+    background-color: #e9ecef;
+    color: #6c757d;
+    cursor: not-allowed;
+  }
+
+  &:not(:disabled):hover {
+    background: ${({ theme }) => theme.colors.primary40};
+    color: white;
+    transition: 0.2s;
+  }
+`;
