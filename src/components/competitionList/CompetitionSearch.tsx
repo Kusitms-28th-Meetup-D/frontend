780a3edf02c6bea14d049b728a6284f65a655785
parkyejin <uiop5809@naver.com>
2023-11-08 16:31:14 +0900

    feat(#8): 공모전 검색 컴포넌트

---
+++ src/components/competitionList/CompetitionSearch.tsx
@@ -0,0 +1,56 @@
+import { styled } from 'styled-components';
+import SearchInput from './SearchInput';
+import CompetitionButton from './CompetitionButton';
+import { useState } from 'react';
+import { searchButtonList } from '../../constants/competitionList';
+
+const CompetitionSearch = () => {
+  const [selectedButton, setSelectedButton] = useState('전체');
+
+  const handleButtonClick = (text: string) => {
+    setSelectedButton(text);
+  };
+
+  return (
+    <CompetitionSearchBox>
+      {/* <SearchBackgroundImg src="/assets/images//searchBox1.svg" /> */}
+      <SearchInput />
+      <CompetitionButtonLayout>
+        {searchButtonList.map((text, index) => (
+          <CompetitionButton
+            key={index}
+            text={text}
+            selected={selectedButton === text}
+            onClick={() => handleButtonClick(text)}
+          />
+        ))}
+      </CompetitionButtonLayout>
+      {/* <SearchBackgroundImg src="/assets/images/competition/searchBox2.svg" /> */}
+    </CompetitionSearchBox>
+  );
+};
+
+export default CompetitionSearch;
+
+const CompetitionSearchBox = styled.div`
+  background: ${({ theme }) => theme.colors.primary40};
+  width: 100%;
+  padding: 3.5rem 2rem;
+  border-radius: 1.2rem;
+  margin: 1.5rem 0;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  gap: 2rem;
+`;
+
+const CompetitionButtonLayout = styled.div`
+  display: flex;
+  flex-wrap: wrap;
+  gap: 1rem;
+  justify-content: center;
+  align-items: center;
+`;
+
+// const SearchBackgroundImg = styled.img``;
