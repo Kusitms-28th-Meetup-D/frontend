aacab662e21cf472c1026f7382adb054b50a049f
j2noo <wlsdndml213@naver.com>
2023-11-08 21:45:51 +0900

    feat(#6) : HeaderSeletedType 정의

--- src/recoil/atom.ts
+++ src/recoil/atom.ts
@@ -1,9 +1,10 @@
 import { atom } from 'recoil';
-export type HeaderColorType = 'transparent' | 'white'; //추가 가능
-export const currHeaderAtom = atom<HeaderColorType>({
-  key: 'currHeader',
-  default: 'white',
-});
+export type HeaderSelectedType =
+  | 'none'
+  | 'list'
+  | 'myTeam'
+  | 'login'
+  | 'mypage'; //추가 가능
 
 export const kakaoNameState = atom({
   key: 'kakaoNameState',
@@ -16,4 +17,8 @@ export const kakaoAccessTokenState = atom({
 export const loginState = atom<boolean>({
   key: 'loginState',
   default: false,
+});
+export const headerSelectedState = atom<HeaderSelectedType>({
+  key: 'headerSelectedState',
+  default: 'none',
 });
