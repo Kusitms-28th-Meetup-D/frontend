fa0b09efdb4e92d42ad5ae2c040596faac91f53b
parkyejin <uiop5809@naver.com>
2023-11-08 16:32:04 +0900

    feat(#8): 공모전 검색 필터링 버튼

--- src/constants/competitionList.ts
+++ src/constants/competitionList.ts
@@ -35,3 +35,60 @@ export const recruitingList = [
     profile: '/assets/images/recommendation/profile.svg',
   },
 ];
+
+export const searchButtonList = [
+  '전체',
+  '기획/아이디어',
+  '브랜드/네이밍',
+  '광고/마케팅',
+  '사진/영상/UCC',
+  '디자인',
+  '예체능',
+  '문학/수기/시나리오',
+  'IT/소프트웨어/게임',
+  '창업/스타트업',
+  '기타',
+];
+
+export const competitionList = [
+  {
+    contestId: 1,
+    title: '제 1회 삼성전자 주니어 소프트웨어 창작대회',
+    images: '/assets/images/competition/competition.svg',
+    teamNum: 8,
+    remainDay: 11,
+    company: '한국경제신문',
+  },
+  {
+    contestId: 2,
+    title: '2024 KT&G 국제 대학생 창업교류전(ASVF) 한국 대표 모집',
+    images: '/assets/images/competition/competition.svg',
+    teamNum: 8,
+    remainDay: 11,
+    company: '한국경제신문',
+  },
+  {
+    contestId: 3,
+    title: '제 1회 삼성전자 주니어 소프트웨어 창작대회',
+    images: '/assets/images/competition/competition.svg',
+    teamNum: 8,
+    remainDay: 11,
+    company: '한국경제신문',
+  },
+  {
+    contestId: 4,
+    title: '제 1회 삼성전자 주니어 소프트웨어 창작대회',
+    images: '/assets/images/competition/competition.svg',
+    teamNum: 8,
+    remainDay: 11,
+    company: '한국경제신문',
+  },
+  {
+    contestId: 5,
+    title: '제 1회 삼성전자 주니어 소프트웨어 창작대회',
+    images: '/assets/images/competition/competition.svg',
+    teamNum: 8,
+    remainDay: 11,
+    company: '한국경제신문',
+  },
+];
--- src/pages/competitionList/CompetitionList.tsx
+++ src/pages/competitionList/CompetitionList.tsx
@@ -1,10 +1,14 @@
 import { styled } from 'styled-components';
 import Recruiting from '../../components/competitionList/CompetitionRecruiting';
+import CompetitionSearch from '../../components/competitionList/CompetitionSearch';
+import CompetitionListBox from '../../components/competitionList/CompetitionListBox';
 
 const CompetitionList = () => {
   return (
     <CompetitionLayout>
       <Recruiting />
+      <CompetitionSearch />
+      <CompetitionListBox />
     </CompetitionLayout>
   );
 };
