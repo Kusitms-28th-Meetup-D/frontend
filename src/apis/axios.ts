2d09928d08825e75d974ca8188cf34afc40230d2
parkyejin <uiop5809@naver.com>
2023-11-17 05:14:44 +0900

    feat(#44): Axios headers Authorization 추가

--- src/apis/axios.ts
+++ src/apis/axios.ts
@@ -1,14 +1,12 @@
 import axios from 'axios';
 
-// const tokenString = sessionStorage.getItem('recoilPersistTest');
-// const token = tokenString ? JSON.parse(tokenString) : null;
-const serviceToken =
-  'eyJhbGciOiJIUzUxMiJ9.eyJzdWIiOiIyIiwiaWF0IjoxNzAwMDcyNDU0LCJleHAiOjE3MDEzNjg0NTR9.zgzovueBE0rsl0KGvVhbdLrYQiTeLSD3Jd9psSdvDqEH-1hiVC-bx01pHdcESGSKRLFogKQBqkycd34MbtEX1Q';
+const tokenString = sessionStorage.getItem('recoilPersistTest');
+const token = tokenString ? JSON.parse(tokenString) : null;
 
 const Axios = axios.create({
   baseURL: 'https://wanteam.site',
   headers: {
-    Authorization: `Bearer ${serviceToken}`,
+    Authorization: `Bearer ${token?.loginState.data.accessToken}`,
     'Content-Type': 'application/json',
   },
 });
