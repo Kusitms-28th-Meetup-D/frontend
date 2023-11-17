d4adb12a909cba337a6be287f7b02e93a96c9756
parkyejin <uiop5809@naver.com>
2023-11-17 23:23:14 +0900

    fix(#44): 검색 전 공모전 리스트

--- src/components/competitionList/CompetitionListBox.tsx
+++ src/components/competitionList/CompetitionListBox.tsx
@@ -1,27 +1,47 @@
 import { styled } from 'styled-components';
 import CompetitionBox from './CompetitionBox';
-import { useCompetitionSearch } from '../../hooks/competition/useCompeitionSearch';
 import { useRecoilValue } from 'recoil';
 import { searchTextAtom } from '../../recoil/competition';
+import { useCompetitionList } from '../../hooks/competition/useCompetitionList';
+import { useCompetitionSearch } from '../../hooks/competition/useCompeitionSearch';
 
 const CompetitionListBox = () => {
   const searchText = useRecoilValue(searchTextAtom);
+
+  const { competitionList } = useCompetitionList();
   const { competitionSearch } = useCompetitionSearch({
     searchText: searchText,
   });
 
   return (
     <CompetitionListLayout>
-      {competitionSearch?.data.map((competition) => (
-        <CompetitionBox
-          contestId={competition.contestId}
-          title={competition.title}
-          images={competition.images}
-          teamNum={competition.teamNum}
-          remainDay={competition.remainDay}
-          company={competition.company}
-        />
-      ))}
+      {searchText.length === 0 ? (
+        <>
+          {competitionList?.data.map((competition) => (
+            <CompetitionBox
+              contestId={competition.contestId}
+              title={competition.title}
+              images={competition.images}
+              teamNum={competition.teamNum}
+              remainDay={competition.remainDay}
+              company={competition.company}
+            />
+          ))}
+        </>
+      ) : (
+        <>
+          {competitionSearch?.data.map((competition) => (
+            <CompetitionBox
+              contestId={competition.contestId}
+              title={competition.title}
+              images={competition.images}
+              teamNum={competition.teamNum}
+              remainDay={competition.remainDay}
+              company={competition.company}
+            />
+          ))}
+        </>
+      )}
     </CompetitionListLayout>
   );
 };
