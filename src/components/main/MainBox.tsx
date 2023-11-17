1f6dda012226c126e1b6ec61ea75a6078db7de36
parkyejin <uiop5809@naver.com>
2023-11-18 06:31:57 +0900

    feat(#59): 메인 페이지 박스

---
+++ src/components/main/MainBox.tsx
@@ -0,0 +1,19 @@
+import { styled } from 'styled-components';
+import LeftBox from './LeftBox';
+import RightBox from './RightBox';
+
+const MainBox = () => {
+  return (
+    <MainBoxContainer>
+      <LeftBox />
+      <RightBox />
+    </MainBoxContainer>
+  );
+};
+
+export default MainBox;
+
+const MainBoxContainer = styled.div`
+  display: flex;
+  gap: 2rem;
+`;
--- src/pages/main/Main.tsx
+++ src/pages/main/Main.tsx
@@ -1,12 +1,30 @@
+import { styled } from 'styled-components';
 import { Headers } from '../../constants/Header';
 import { headerSelectedState } from '../../recoil/atom';
 import { useEffect } from 'react';
 import { useSetRecoilState } from 'recoil';
+import MainBox from '../../components/main/MainBox';
 
 const Main = () => {
   const setHeaderSelected = useSetRecoilState(headerSelectedState);
   useEffect(() => setHeaderSelected(Headers.none));
 
-  return <div>main1!!</div>;
+  return (
+    <MainLayout>
+      <MainBox />
+    </MainLayout>
+  );
 };
 export default Main;
+
+const MainLayout = styled.div`
+  margin: 0 auto;
+
+  @media ${({ theme }) => theme.devices.mobile} {
+  }
+  @media ${({ theme }) => theme.devices.tablet} {
+  }
+  @media ${({ theme }) => theme.devices.desktop} {
+    max-width: 122.4rem;
+  }
+`;
