4102c5f1db8357ea048ea72a3818e9e0f0ede9fe
j2noo <wlsdndml213@naver.com>
2023-11-18 04:06:19 +0900

    fix(#53) : 안쓰는 constant 지우기

--- src/constants/Profile.ts
+++ src/constants/Profile.ts
@@ -1,3 +1,5 @@
+import { ICategory } from '../interface/Profile';
+
 // export const ProfileSubInfoData: IProfileSubInfoData = {
 //   activitiesData: {
 //     title: '대외활동 및 인턴',
@@ -33,46 +35,46 @@
 //   },
 // };
 
-// export const TEAM_CURTURE_CATEGORY: ICategory = {
-//   title: '팀 문화',
-//   category: [
-//     {
-//       subtitle: '피드백 스타일',
-//       leftValue: '솔직한 편',
-//       rightValue: '돌려 말하는 편',
-//     },
-//     {
-//       subtitle: '업무 스타일',
-//       leftValue: '업무에 집중',
-//       rightValue: '친목과 업무를 함께',
-//     },
-//     {
-//       subtitle: '에너지 방향',
-//       leftValue: '외향적',
-//       rightValue: '내향적',
-//     },
-//   ],
-// };
-// export const WORK_METHOD_CATEGORY: ICategory = {
-//   title: '작업 방식',
-//   category: [
-//     {
-//       subtitle: '작업 스타일',
-//       leftValue: '신중하게 작업',
-//       rightValue: '속도감 있게 작업',
-//     },
-//     {
-//       subtitle: '결과와 과정',
-//       leftValue: '결과주의',
-//       rightValue: '과정주의',
-//     },
-//     {
-//       subtitle: '일과 삶의 균형',
-//       leftValue: '워커홀릭',
-//       rightValue: '워라밸 중시',
-//     },
-//   ],
-// };
+export const TEAM_CURTURE_CATEGORY: ICategory = {
+  title: '팀 문화',
+  category: [
+    {
+      subtitle: '피드백 스타일',
+      leftValue: '솔직한 편',
+      rightValue: '돌려 말하는 편',
+    },
+    {
+      subtitle: '업무 스타일',
+      leftValue: '업무에 집중',
+      rightValue: '친목과 업무를 함께',
+    },
+    {
+      subtitle: '에너지 방향',
+      leftValue: '외향적',
+      rightValue: '내향적',
+    },
+  ],
+};
+export const WORK_METHOD_CATEGORY: ICategory = {
+  title: '작업 방식',
+  category: [
+    {
+      subtitle: '작업 스타일',
+      leftValue: '신중하게 작업',
+      rightValue: '속도감 있게 작업',
+    },
+    {
+      subtitle: '결과와 과정',
+      leftValue: '결과주의',
+      rightValue: '과정주의',
+    },
+    {
+      subtitle: '일과 삶의 균형',
+      leftValue: '워커홀릭',
+      rightValue: '워라밸 중시',
+    },
+  ],
+};
 
 export const profileCreateInfo = [
   {
