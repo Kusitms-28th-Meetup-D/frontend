d75ae84bb306400ed07c88819e5b468f68ae8e27
j2noo <wlsdndml213@naver.com>
2023-11-19 23:47:05 +0900

    feat(#75) : 지원한 팀 완료

--- src/interface/MyTeam.ts
+++ src/interface/MyTeam.ts
@@ -4,6 +4,26 @@ import { ProfileProps } from './Contest';
 export interface RequestAppliedTeam {
   // contestId: string;
 }
+export interface AppliedTeamData {
+  contestId: string;
+  contestTitle: string;
+  contestImage: string[];
+  leaderInfo: ProfileProps;
+  teamId: number;
+  status: string;
+  leaderMessage: string;
+  max: number;
+  cur: number;
+  location: string;
+  endDate: string;
+}
+export interface ResponseAppliedTeam {
+  status: number;
+  message: string;
+  data: AppliedTeamData[];
+}
+
+//예진이가 사용한 오픈한 팀 api
 export interface TeamData {
   teamId?: number;
   teamMemberSize?: number;
@@ -13,12 +33,6 @@ export interface TeamData {
   contestTitle?: string;
   contestImage: string[];
 }
-export interface ResponseAppliedTeam {
-  status: number;
-  message: string;
-  data: TeamData[];
-}
-
 export interface ResponseOpendTeam {
   status: number;
   message: string;
