ac11c0bd750de04338119023b9b7ce443e4a3f33
j2noo <wlsdndml213@naver.com>
2023-11-19 09:43:39 +0900

    feat(#63) : 오픈된 팀  상세보기 페이지 레이아웃

---
+++ src/components/contestTeam/TeamMembers.tsx
@@ -0,0 +1,78 @@
+import styled from 'styled-components';
+import { ProfileBoxProps, ProfileProps } from '../../interface/Contest';
+import ProfileBoxMember from '../common/ProfileBoxMember';
+
+const TeamMembers = ({
+  memberDatas,
+  leftMember,
+  cur,
+  max,
+}: {
+  memberDatas?: ProfileProps[];
+  leftMember?: number;
+  cur?: number;
+  max?: number;
+}) => {
+  return (
+    <TeamMembersLayout>
+      <TeamMembersTitle>
+        팀원들
+        <TeamMembersLeft>
+          <span>{leftMember}</span>자리 남았어요!
+        </TeamMembersLeft>
+      </TeamMembersTitle>
+      <TeamMembersLeftInfo>
+        지금까지 정원 <span>{max}</span>명 중 <span>{cur}</span>명의 팀원이
+        합류했어요.
+      </TeamMembersLeftInfo>
+      <TeamMembersProfileContainer>
+        {memberDatas?.map((memberData, index) => {
+          const teamMemberDataProps: ProfileBoxProps = {
+            hasProfileButton: false,
+            isBgColorWhite: false,
+            hasBorder: true,
+            memberInfo: memberData,
+            width: 20,
+            height: 27.6,
+          };
+          return <ProfileBoxMember {...teamMemberDataProps} key={index} />;
+        })}
+      </TeamMembersProfileContainer>
+    </TeamMembersLayout>
+  );
+};
+const TeamMembersLayout = styled.div`
+  width: 100%;
+`;
+const TeamMembersTitle = styled.div`
+  ${(props) => props.theme.fonts.heading3};
+  color: ${(props) => props.theme.colors.gray100};
+
+  display: flex;
+  /* justify-content: center; */
+  align-items: center;
+  gap: 1.6rem;
+`;
+const TeamMembersLeft = styled.div`
+  ${(props) => props.theme.fonts.bodyM};
+  color: ${(props) => props.theme.colors.gray100};
+
+  border: 1px solid ${(props) => props.theme.colors.error20};
+  border-radius: 0.8rem;
+  padding: 0.4rem 1.2rem;
+  span {
+    ${(props) => props.theme.fonts.subtitleM};
+    color: ${(props) => props.theme.colors.error90};
+  }
+`;
+const TeamMembersLeftInfo = styled.div`
+  ${(props) => props.theme.fonts.bodyXL};
+  color: ${(props) => props.theme.colors.gray80};
+
+  span {
+    ${(props) => props.theme.fonts.subtitleXL};
+    color: ${(props) => props.theme.colors.primary60};
+  }
+`;
+const TeamMembersProfileContainer = styled.div``;
+export default TeamMembers;
