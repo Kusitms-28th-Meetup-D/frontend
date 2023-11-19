189e48bc49d9af3cd0369f48716c61714ccea47f
parkyejin <uiop5809@naver.com>
2023-11-19 15:15:40 +0900

    docs(#65): 외부 추천사 페이지

--- src/Router.tsx
+++ src/Router.tsx
@@ -18,6 +18,7 @@ import ScrollToTop from './components/common/ScrollToTop';
 import MyTeam from './pages/myteam/MyTeam';
 import MyTeamOpen from './components/myteam/MyTeamOpen';
 import ContestTeam from './pages/contestTeam/ContestTeam';
+import ExternalReview from './pages/recommendation/ExternalReview';
 
 function Router() {
   return (
@@ -30,27 +31,34 @@ function Router() {
         <Route path="/login/oauth" element={<Oauth />} />
         <Route path="/join" element={<Join />} />
         <Route path="/join/request" element={<Request />} />
+
         <Route path="/recommendation" element={<Recommendation />}>
           <Route path="subjective" element={<Subjective />} />
           <Route path="multipleChoice" element={<MultipleChoice />} />
         </Route>
+        <Route path="/review/external" element={<ExternalReview />} />
+
         <Route path="/profile/:userId" element={<Profile />} />
         <Route path="/profile/:userId/create" element={<ProfileCreate />} />
+
         <Route path="/myteam/:userId" element={<MyTeam />}>
           <Route path="open" element={<MyTeamOpen />} />
           <Route path="apply" element={<div>지원한 팀</div>} />
           <Route path="active" element={<div>활동중인 팀</div>} />
           <Route path="end" element={<div>활동했던 팀</div>} />
         </Route>
+
         <Route path="/list/:contestId" element={<Contest />} />
         <Route path="/list/:contestId" element={<Contest />} />
         <Route path="/list/:contestId/:teamId" element={<ContestTeam />} />
+
         <Route path="/profile/create" element={<ProfileCreate />} />
         <Route
           path="/list/:postId/:teamId"
           element={<div>팀 생성페이지입니다</div>}
         />
         <Route path="/list" element={<CompetitionList />} />
+
         <Route path="/payment/charge" element={<PaymentCharge />} />
       </Routes>
       <Footer />
---
+++ src/pages/recommendation/ExternalReview.tsx
@@ -0,0 +1,12 @@
+import { styled } from 'styled-components';
+
+const ExternalReview = () => {
+  return <ExternalReviewContainer></ExternalReviewContainer>;
+};
+
+export default ExternalReview;
+
+const ExternalReviewContainer = styled.div`
+  margin: 0 auto;
+  max-width: 122.4rem;
+`;
