7e61bd7f5b9f8217143a5a037c92eee58031b845
parkyejin <uiop5809@naver.com>
2023-11-19 12:30:46 +0900

    feat(#59): 메인페이지 반응형 제외

--- src/pages/main/Main.tsx
+++ src/pages/main/Main.tsx
@@ -29,12 +29,5 @@ export default Main;
 
 const MainLayout = styled.div`
   margin: 0 auto;
-
-  @media ${({ theme }) => theme.devices.mobile} {
-  }
-  @media ${({ theme }) => theme.devices.tablet} {
-  }
-  @media ${({ theme }) => theme.devices.desktop} {
-    max-width: 122.4rem;
-  }
+  max-width: 122.4rem;
 `;
