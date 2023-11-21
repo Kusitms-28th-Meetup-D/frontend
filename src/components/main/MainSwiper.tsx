54b0d6e3da64eff8833cfb14cec6bc0fac758f7d
j2noo <wlsdndml213@naver.com>
2023-11-22 07:18:27 +0900

    swiper test

--- src/components/main/MainSwiper.tsx
+++ src/components/main/MainSwiper.tsx
@@ -1,179 +1,108 @@
+// Import Swiper React components
 import { Swiper, SwiperSlide } from 'swiper/react';
+
+// Import Swiper styles
 import 'swiper/css';
 import 'swiper/css/pagination';
 import 'swiper/css/navigation';
-import 'swiper/css/scrollbar';
 
-import { Autoplay, Scrollbar, Navigation } from 'swiper/modules';
-
+// import required modules
+import { Autoplay, Pagination, Navigation } from 'swiper/modules';
 import styled from 'styled-components';
 
-import leftSrc from '/assets/images/common/left-arrow.svg';
-import rightSrc from '/assets/images/common/right-arrow.svg';
+const SwiperContainer = styled(Swiper)`
+  /* background-color: red; */
+  width: 100vw;
+  height: 100vh;
+
+  @media screen and (max-width: 500px) {
+    height: 100vw;
+  }
+  /* 
+  .swiper-button-prev {
+
+  .swiper-button-next::after,
+  .swiper-button-prev::after {
+    display: none;
+  } */
+  //pagination bar에 적용하는 스타일 -활성화된 bullet
+
+  .swiper-pagination {
+    margin-bottom: calc(100px - 10px);
+
+    //모바일
+    @media screen and (max-width: 500px) {
+      position: absolute;
+      /* transform: translateY(270px); */
+      top: 270px;
+    }
+  }
+  //pagination bar에 적용하는 스타일 - 전체 bullet
+  .swiper-pagination-bullet {
+    width: 16px;
+    height: 16px;
+    margin: 0 30px !important;
+    text-align: center;
+    line-height: 20px;
+    font-size: 18px;
+
+    background: #9cb9ca; //선택안된거
+    opacity: 1;
+    /* background: transparent; */
+    transition: background 0.3s ease;
+
+    &:hover {
+      color: #000;
+    }
+    //모바일
+    @media screen and (max-width: 500px) {
+      width: 10px;
+      height: 10px;
+      margin: 0 10px !important;
+    }
+  }
+  .swiper-pagination-bullet-active {
+    color: #000;
+
+    background: #007aff;
+  }
+`;
+
 const MainSwiper = () => {
   return (
     <>
-      <SwiperStyle
+      <SwiperContainer
         loop={true}
+        spaceBetween={0}
+        // centeredSlides={true}
         autoplay={{
-          delay: 3000,
+          delay: 1000,
           disableOnInteraction: false,
         }}
-        scrollbar={{
-          hide: false, // 스크롤바를 숨기지 않음
-          draggable: true, // 사용자가 스크롤바를 직접 조작 가능
+        pagination={{
+          clickable: true,
         }}
-        navigation={{
-          nextEl: '.swiper-button-next',
-          prevEl: '.swiper-button-prev',
-        }}
-        modules={[Autoplay, Navigation, Scrollbar]}
+        // navigation={true}
+        modules={[Autoplay, Pagination, Navigation]}
         className="mySwiper"
       >
-        <div className="swiper-button-prev"></div>
-        <div className="swiper-button-next"></div>
-        <SwiperSlideStyle>
-          <SwiperContainer>
-            <SwiperLeft>
-              <p>원팀이 처음이세요?</p>
-              <h1>원팀에서 원하는 팀원을</h1>
-              <h1>한번에 찾아보세요!</h1>
-            </SwiperLeft>
-            <SwiperImg
-              src={'/assets/images/main/swiper1.svg'}
-              alt={'swiper_img'}
-            />
-          </SwiperContainer>
-        </SwiperSlideStyle>
-        <SwiperSlideStyle>
-          <SwiperContainer>
-            <SwiperLeft>
-              <p>원팀이 처음이세요?</p>
-              <h1>원팀에서 원하는 팀원을</h1>
-              <h1>한번에 찾아보세요!</h1>
-            </SwiperLeft>
-            <SwiperImg
-              src={'/assets/images/main/swiper2.svg'}
-              alt={'swiper_img'}
-            />
-          </SwiperContainer>
-        </SwiperSlideStyle>
-        <SwiperSlideStyle>
-          <SwiperContainer>
-            <SwiperLeft>
-              <p>나는 어떤 팀원일까?</p>
-              <h1>나와 함께 했던 팀원들이</h1>
-              <h1>남겨준 리뷰를 확인하세요</h1>
-            </SwiperLeft>
-            <SwiperImg
-              src={'/assets/images/main/swiper3.svg'}
-              alt={'swiper_img'}
-            />
-          </SwiperContainer>
-        </SwiperSlideStyle>
-        <SwiperSlideStyle>
-          <SwiperContainer>
-            <SwiperLeft>
-              <p>내가 추천하고 싶은 팀원은요</p>
-              <h1>최고의 팀원에게 남겨주는</h1>
-              <h1>리뷰를 확인하세요</h1>
-            </SwiperLeft>
-            <SwiperImg
-              src={'/assets/images/main/swiper4.svg'}
-              alt={'swiper_img'}
-            />
-          </SwiperContainer>
-        </SwiperSlideStyle>
-      </SwiperStyle>
+        <SwiperSlide>
+          <div>11</div>
+        </SwiperSlide>
+        <SwiperSlide>
+          <div>222222</div>
+        </SwiperSlide>
+        <SwiperSlide>
+          <div>133333333</div>
+        </SwiperSlide>
+        <SwiperSlide>
+          <div>144444444444</div>
+        </SwiperSlide>
+        <SwiperSlide>
+          <div>1555555555551</div>
+        </SwiperSlide>
+      </SwiperContainer>
     </>
   );
 };
-
 export default MainSwiper;
-
-const SwiperStyle = styled(Swiper)`
-  position: relative;
-  z-index: 0;
-
-  .swiper-scrollbar {
-    position: absolute;
-    left: calc(50% - 61.2rem);
-    top: 70%;
-    transform: translateY(-50%);
-    width: 15.4rem;
-    height: 0.8rem;
-
-    background-color: white;
-    /* position: absolute; */
-    z-index: 1;
-  }
-  .swiper-scrollbar-drag {
-    background-color: #5c5ffa; /* 드래그 상태일 때 색상 조절 */
-  }
-
-  .swiper-button-next,
-  .swiper-button-prev {
-    position: absolute;
-    top: calc(70% + 2rem);
-    transform: translateY(-50%);
-    width: 1rem;
-    height: 2rem;
-    /* background-color: red; */
-    /* border: 1px solid red; */
-  }
-
-  // 이전 버튼
-  .swiper-button-prev {
-    left: calc(50% - 61.2rem + 15.4rem + 2rem);
-
-    background: url(${leftSrc}) no-repeat;
-    background-size: cover;
-    background-position: center;
-  }
-  //다음 버튼
-  .swiper-button-next {
-    left: calc(50% - 61.2rem + 15.4rem + 2rem + 3rem);
-
-    background: url(${rightSrc}) no-repeat;
-    background-size: cover;
-    background-position: center;
-  }
-
-  .swiper-button-prev::after,
-  .swiper-button-next::after {
-    display: none;
-  }
-`;
-
-const SwiperSlideStyle = styled(SwiperSlide)`
-  height: 48rem;
-  background-image: url('/assets/images/main/swiper_background.svg');
-  background-size: cover;
-`;
-
-const SwiperContainer = styled.div`
-  width: 122.4rem;
-  margin: 0 auto;
-  padding: 6rem 0 4rem 0;
-  display: flex;
-  justify-content: space-between;
-`;
-
-const SwiperLeft = styled.div`
-  p {
-    ${({ theme }) => theme.fonts.bodyXL};
-    color: ${({ theme }) => theme.colors.white};
-    margin-bottom: 1rem;
-  }
-  h1 {
-    ${({ theme }) => theme.fonts.heading2};
-    color: ${({ theme }) => theme.colors.white};
-  }
-`;
-
-const SwiperImg = styled.img`
-  transform: translateY(-4rem);
-  width: 40rem;
-  height: 38.3rem;
-  margin-right: 5rem;
-`;
