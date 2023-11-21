ebdb80ecdd64c3a8c5a293f56f6ea4143c7ac5ea
parkyejin <uiop5809@naver.com>
2023-11-22 01:34:49 +0900

    docs(#98): 프로필 - 이력 title text 변경

--- src/components/profile/ProfileSubInfo.tsx
+++ src/components/profile/ProfileSubInfo.tsx
@@ -2,41 +2,6 @@ import styled from 'styled-components';
 import { useState } from 'react';
 import { useProfileCreate } from '../../hooks/profile/useProfileCreate';
 
-/*
-const activitiesData: SubInfoContentsProps = {
-  title: '대외활동 및 인턴',
-  contents: [
-    '한국대학생IT경영학회 (23.08~): 기획 파트, 팀원 모집 플랫폼 "Wanteam." 기획',
-    '캐캐오기업 (22.01~22.12): 제품 기획 부서, 초콜릿 신제품 맛보기 테스트',
-    '코코넛기업 (21.06~21.09): 영업 부서, 코코넛 사세요 맛 좋은 코코넛 사세요',
-  ],
-};
-const awardsData: SubInfoContentsProps = {
-  title: '수상 경력',
-  contents: [
-    '초콜릿 맛있게 먹기 공모전 최우수상',
-    '샘송 광고 기획 아이디어 공모전 장려상',
-    '멍 때리기 대회 대상',
-  ],
-};
-const toolsData: SubInfoContentsProps = {
-  title: '사용 가능 툴',
-  contents: [
-    '노션: 휘황찬란하게 활용하지는 못하지만 적당히 쓸 줄은 알아요!',
-    '지라: 인턴할 때 써봐서 어느 정도 활용 가능합니다.',
-    '피그마: 간단한 와이어프레임 정도는 그릴 수 있어요! ',
-  ],
-};
-const licensesData: SubInfoContentsProps = {
-  title: '대외활동 및 인턴',
-  contents: [
-    '늦게 자기 자격증 1급',
-    '해리 포터 덕후 자격증 마스터',
-    '(G)-IDLE 팬클럽 ‘네버랜드’ 4기',
-  ],
-};
-*/
-
 const ProfileSubInfo = ({
   internships,
   awards,
@@ -48,12 +13,7 @@ const ProfileSubInfo = ({
   tools?: string[];
   certificates?: string[];
 }) => {
-  const TITLES = [
-    '대외활동 및 인턴',
-    '수상 경력',
-    '사용 가능 툴',
-    '보유 자격증',
-  ];
+  const TITLES = ['대외활동', '인턴', '사용 가능 툴', '보유 자격증'];
   const NAMES = ['internships', 'awards', 'tools', 'certificates'];
 
   const [isModifying, setIsModyfying] = useState(false);
